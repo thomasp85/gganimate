2af33f7c2d10a3b1718a091ccf3e8c2b630aaeb2
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-12-10 11:46:16 +0100

    Fix #184 deprecate id in transition_components and transition_reveal

--- R/transition-components.R
+++ R/transition-components.R
@@ -5,14 +5,13 @@
 #' "state" and "transition" phase as any component can be moving or static at
 #' any point in time.
 #'
-#' @param id The unquoted name of the column holding the id that links
-#' components across the data
 #' @param time The unquoted name of the column holding the time for each state
 #' of the components
 #' @param range The range the animation should span. Defaults to the range of
 #' time plus enter and exit length
 #' @param enter_length,exit_length How long time should be spend on enter and
 #' exit transitions. Defaults to 0
+#' @param id **Deprecated**
 #'
 #' @section Label variables:
 #' `transition_components` makes the following variables available for string
@@ -25,14 +24,12 @@
 #' @export
 #' @importFrom rlang enquo
 #' @importFrom ggplot2 ggproto
-transition_components <- function(id, time, range = NULL, enter_length = NULL, exit_length = NULL) {
-  id_quo <- enquo(id)
+transition_components <- function(time, range = NULL, enter_length = NULL, exit_length = NULL, id) {
+  if (!missing(id)) warning('The `id` argument has been deprecated. Set `id` in each layer with the `group` aesthetic', call. = FALSE)
   time_quo <- enquo(time)
-  require_quo(id_quo, 'id')
   require_quo(time_quo, 'time')
   ggproto(NULL, TransitionComponents,
           params = list(
-            id_quo = id_quo,
             time_quo = time_quo,
             range = range,
             enter_length = enter_length,
@@ -48,36 +45,30 @@ transition_components <- function(id, time, range = NULL, enter_length = NULL, e
 #' @importFrom stringi stri_match
 #' @importFrom tweenr tween_components
 TransitionComponents <- ggproto('TransitionComponents', Transition,
-  mapping = '(.+?)-(.+)',
-  var_names = c('time', 'id'),
+  mapping = '(.+?)',
+  var_names = 'time',
   setup_params = function(self, data, params) {
-    params$id <- get_row_id(data, params$id_quo)
     params$time <- get_row_comp_time(data, params$time_quo, params)
-    params$require_stat <- is_placeholder(params$id) || is_placeholder(params$time)
-    static <- lengths(params$id$values) == 0 || lengths(params$time$values) == 0
-    params$row_id <- Map(function(t, i, s) if (s) character() else paste0(t, '-', i),
-                         t = params$time$values, i = params$id$values, s = static)
+    params$require_stat <- is_placeholder(params$time)
+    static <- lengths(params$time$values) == 0
+    params$row_id <- Map(function(t, s) if (s) character() else t,
+                         t = params$time$values, s = static)
     params
   },
   setup_params2 = function(self, data, params, row_vars) {
-    if (is_placeholder(params$id)) {
-      params$id <- get_row_id(data, params$id_quo, after = TRUE)
-    } else {
-      params$id$values <- row_vars$id
-    }
     if (is_placeholder(params$time)) {
       params$time <- get_row_comp_time(data, params$time_quo, params, after = TRUE)
     } else {
       params$time$values <- row_vars$time
     }
-    static <- lengths(params$id$values) == 0 || lengths(params$time$values) == 0
+    static <- lengths(params$time$values) == 0
 
     params$enter_length <- params$time$enter_length
     params$exit_length <- params$time$exit_length
     params$range <- params$time$range
     params$frame_time <- params$time$frame_time
-    params$row_id <- Map(function(t, i, s) if (s) character() else paste0(t, '-', i),
-                         t = params$time$values, i = params$id$values, s = static)
+    params$row_id <- Map(function(t, s) if (s) character() else t,
+                         t = params$time$values, s = static)
     params$frame_info <- data.frame(
       frame_time = params$time$frame_time
     )
@@ -89,10 +80,9 @@ TransitionComponents <- ggproto('TransitionComponents', Transition,
     if (is.null(row_vars)) return(data)
     data$group <- paste0(row_vars$before, row_vars$after)
     time <- as.numeric(row_vars$time)
-    id <- row_vars$id
     all_frames <- switch(
       type,
-      point = tween_components(data, ease, params$nframes, !!time, !!id, c(1, params$nframes), enter, exit, params$enter_length, params$exit_length),
+      point = tween_components(data, ease, params$nframes, !!time, group, c(1, params$nframes), enter, exit, params$enter_length, params$exit_length),
       stop("Unsupported layer type", call. = FALSE)
     )
     all_frames$group <- paste0(all_frames$group, '<', all_frames$.frame, '>')
@@ -100,18 +90,6 @@ TransitionComponents <- ggproto('TransitionComponents', Transition,
     all_frames
   }
 )
-get_row_id <- function(data, quo, after = FALSE) {
-  if (after || !require_stat(quo[[2]])) {
-    list(values = lapply(data, function(d) {
-      id <- safe_eval(d, expr = quo)
-      if (is.null(id)) return(id)
-      id <- as.character(id)
-      match(id, unique(id))
-    }))
-  } else {
-    eval_placeholder(data)
-  }
-}
 get_row_comp_time <- function(data, quo, params, after = FALSE) {
   if (!after && require_stat(quo[[2]])) {
     return(eval_placeholder(data))
@@ -144,8 +122,11 @@ get_row_comp_time <- function(data, quo, params, after = FALSE) {
     as.numeric(params$range)
   }
   full_length <- diff(range)
-  row_frame <- lapply(standard_times$times, function(x) {
-    round((params$nframes - 1) * (x - range[1])/full_length) + 1
+  frames <- lapply(standard_times$times, function(x) {
+    if (is.null(x)) return(numeric())
+    frame <- round((params$nframes - 1) * (x - range[1])/full_length) + 1
+    nc <- nchar(max(frame))
+    sprintf(paste0('%0', nc, 'i'), frame)
   })
   frame_time <- recast_times(
     seq(range[1], range[2], length.out = params$nframes),
@@ -153,7 +134,7 @@ get_row_comp_time <- function(data, quo, params, after = FALSE) {
   )
   frame_length <- full_length / params$nframes
   list(
-    values = row_frame,
+    values = frames,
     frame_time = frame_time,
     enter_length = round(enter_length / frame_length),
     exit_length = round(exit_length / frame_length)
--- R/transition_reveal.R
+++ R/transition_reveal.R
@@ -8,8 +8,6 @@
 #' disconnected segments as will happen when using [transition_time()] and
 #' [shadow_mark()] together.
 #'
-#' @param id An unquoted expression giving the id of the row (usually the same
-#' as the group aesthetic for lines and polygons)
 #' @param along An unquoted expression giving the dimension to tween along. For
 #' a gradually revealing time series this should be set to the same as the `x`
 #' aesthetic.
@@ -17,6 +15,7 @@
 #' of `along`
 #' @param keep_last For non-path/polygon layers should the last row be kept for
 #' subsequent frames.
+#' @param id **Deprecated**
 #'
 #' @section Label variables:
 #' `transition_along` makes the following variables available for string
@@ -31,19 +30,17 @@
 #'
 #' p <- ggplot(airquality, aes(Day, Temp, group = Month)) +
 #'   geom_line() +
-#'   transition_reveal(Month, Day)
+#'   transition_reveal(Day)
 #'
 #' # animate(p)
 #'
 #' @export
-transition_reveal <- function(id, along, range = NULL, keep_last = TRUE) {
-  id_quo <- enquo(id)
+transition_reveal <- function(along, range = NULL, keep_last = TRUE, id) {
+  if (!missing(id)) warning('The `id` argument has been deprecated. Set `id` in each layer with the `group` aesthetic', call. = FALSE)
   along_quo <- enquo(along)
-  require_quo(id_quo, 'id')
   require_quo(along_quo, 'along')
   ggproto(NULL, TransitionReveal,
           params = list(
-            id_quo = id_quo,
             along_quo = along_quo,
             range = range,
             keep_last = keep_last
@@ -58,44 +55,39 @@ transition_reveal <- function(id, along, range = NULL, keep_last = TRUE) {
 #' @importFrom stringi stri_match
 #' @importFrom tweenr tween_along
 TransitionReveal <- ggproto('TransitionReveal', Transition,
-  mapping = '(.+?)_(.+)',
-  var_names = c('id', 'along'),
+  mapping = '(.+)',
+  var_names = 'along',
   setup_params = function(self, data, params) {
-    params$id <- get_row_id(data, params$id_quo)
     params$along <- get_row_along(data, params$along_quo, params$nframes, params$range)
-    params$require_stat <- is_placeholder(params$id) || is_placeholder(params$along)
-    params$row_id <- Map(function(t, i) if (length(t) == 0 || length(i) == 0) character() else paste0(i, '_', format(t)),
-                         t = params$along$values, i = params$id$values)
+    params$require_stat <- is_placeholder(params$along)
+    static <- lengths(params$along$values) == 0
+    params$row_id <- Map(function(t, s) if (s) character() else t,
+                         t = params$along$values, s = static)
     params
   },
   setup_params2 = function(self, data, params, row_vars) {
-    if (is_placeholder(params$id)) {
-      params$id <- get_row_id(data, params$id_quo, after = TRUE)
-    } else {
-      params$id$values <- row_vars$id
-    }
     if (is_placeholder(params$along)) {
       params$along <- get_row_along(data, params$along_quo, params$nframes, params$range, after = TRUE)
     } else {
       params$along$values <- row_vars$along
     }
-    params$row_id <- Map(function(t, i) if (length(t) == 0 || length(i) == 0) character() else paste0(i, '_', format(t)),
-                         t = params$along$values, i = params$id$values)
+    static <- lengths(params$along$values) == 0
+    params$row_id <- Map(function(t, s) if (s) character() else t,
+                         t = params$along$values, s = static)
     params$frame_info <- data.frame(frame_along = params$along$frame_time)
     params
   },
   expand_panel = function(self, data, type, id, match, ease, enter, exit, params, layer_index) {
-    split_panel <- stri_match(data$group, regex = '^(.+)<(.+?)_(.+)>(.*)$')
-    if (is.na(split_panel[1])) return(data)
-    data$group <- paste0(split_panel[, 2], split_panel[, 5])
-    time <- as.numeric(split_panel[, 4])
-    id <- split_panel[,3]
+    row_vars <- self$get_row_vars(data)
+    if (is.null(row_vars)) return(data)
+    data$group <- paste0(row_vars$before, row_vars$after)
+    time <- as.numeric(row_vars$along)
 
     all_frames <- switch(
       type,
-      point = tween_along(data, ease, params$nframes, !!time, !!id, c(0, 1), FALSE, params$keep_last),
-      path = tween_along(data, ease, params$nframes, !!time, !!id, c(0, 1), TRUE, params$keep_last),
-      polygon = tween_along(data, ease, params$nframes, !!time, !!id, c(0, 1), TRUE, params$keep_last),
+      point = tween_along(data, ease, params$nframes, !!time, group, c(1, params$nframes), FALSE, params$keep_last),
+      path = tween_along(data, ease, params$nframes, !!time, group, c(1, params$nframes), TRUE, params$keep_last),
+      polygon = tween_along(data, ease, params$nframes, !!time, group, c(1, params$nframes), TRUE, params$keep_last),
       stop("Unsupported layer type", call. = FALSE)
     )
     all_frames$group <- paste0(all_frames$group, '<', all_frames$.frame, '>')
@@ -123,11 +115,14 @@ get_row_along <- function(data, quo, nframes, range, after = FALSE) {
     }
     range <- as.numeric(range)
   }
-  times <- lapply(times, function(v) {
+  full_length <- diff(range)
+  frames <- lapply(times, function(v) {
     if (is.null(v)) return(numeric())
-    (v - range[1]) / diff(range)
+    frame <- round((nframes - 1) * (v - range[1])/full_length) + 1
+    nc <- nchar(max(frame))
+    sprintf(paste0('%0', nc, 'i'), frame)
   })
   frame_time <- seq(range[1], range[2], length.out = nframes)
   frame_time <- recast_times(frame_time, time_class)
-  list(values = times, frame_time = frame_time)
+  list(values = frames, frame_time = frame_time)
 }
