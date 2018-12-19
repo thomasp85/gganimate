fffd0eb8456300ee4eeb9cbc74a74722f9a367ca
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-12-19 14:56:43 +0100

    Dont render examples in view_zoom

--- R/view-zoom.R
+++ R/view-zoom.R
@@ -32,14 +32,14 @@ NULL
 #'
 #' # Use pan_zoom to change the relationship between pan- and zoom movement
 #' # Mainly zooming
-#' ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
+#' anim1 <- ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
 #'   geom_point() +
 #'   transition_states(Species, transition_length = 2, state_length = 1) +
 #'   shadow_mark(past = TRUE, future = TRUE, colour = 'grey') +
 #'   view_zoom(pause_length = 1, step_length = 2, nsteps = 3, pan_zoom = -3)
 #'
 #' # Mainly panning
-#' ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
+#' anim2 <- ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
 #'   geom_point() +
 #'   transition_states(Species, transition_length = 2, state_length = 1) +
 #'   shadow_mark(past = TRUE, future = TRUE, colour = 'grey') +
