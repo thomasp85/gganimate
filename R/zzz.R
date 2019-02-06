563623fa81e86ee911dab8e95a78aa9535ded2a5
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-02-06 10:27:54 +0100

    Use delayed S3 registration for knitr hooks

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+if (getRversion() >= "3.6.0") {
+  S3method(knitr::knit_print, 'gganim')
+  S3method(knitr::knit_print, 'gif_image')
+  S3method(knitr::knit_print, 'video_file')
+  S3method(knitr::knit_print, 'sprite_image')
+}
