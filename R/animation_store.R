efcf8d67dfc8f252208407fa1f9f6e8022346b76
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-03-12 10:42:42 +0100

    Fix #416 export set_last_animation()

--- R/animation_store.R
+++ R/animation_store.R
@@ -1,8 +1,10 @@
 .store <- new.env(parent = emptyenv())
-set_last_animation <- function(value) .store$animation <- value
 
 #' Retrieve the last rendered animation
 #'
 #' @export
 #' @keywords internal
 last_animation <- function() .store$animation
+#' @rdname last_animation
+#' @export
+set_last_animation <- function(value) .store$animation <- value
