d4e17d230b492552ec1593b2104e4d3e8556fa80
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-03-15 08:42:15 +0100

    fix #406 improve transformr error messages

--- R/transformr.R
+++ R/transformr.R
@@ -1,18 +1,18 @@
 transform_path <- function(...) {
   if (!requireNamespace('transformr', quietly = TRUE)) {
-    stop('transformr is required to tween paths and lines')
+    stop('The transformr package is required to tween paths and lines')
   }
   transformr::tween_path(...)
 }
 transform_polygon <- function(...) {
   if (!requireNamespace('transformr', quietly = TRUE)) {
-    stop('transformr is required to tween polygons')
+    stop('The transformr package is required to tween polygons')
   }
   transformr::tween_polygon(...)
 }
 transform_sf <- function(...) {
   if (!requireNamespace('transformr', quietly = TRUE)) {
-    stop('transformr is required to tween sf layers')
+    stop('The transformr package is required to tween sf layers')
   }
   transformr::tween_sf(...)
 }
