b4267a97243bd185cd6ee68ec8daab1c5ba447d2
Thomas Lin Pedersen <thomasp85@gmail.com>
2023-08-30 10:11:21 +0200

    Fix #485

--- R/animate.R
+++ R/animate.R
@@ -210,7 +210,7 @@ gg_animate <- gganimate
 #' @importFrom utils modifyList
 prepare_args <- function(nframes, fps, duration, detail, renderer, device, ref_frame, start_pause, end_pause, rewind, ...) {
   args <- list()
-  chunk_args <- if (is_knitting()) get_knitr_options(knitr::opts_chunk$get(), unlist = FALSE) else list(dev_args = list())
+  chunk_args <- if (is_knitting()) get_knitr_options(knitr::opts_current$get(), unlist = FALSE) else list(dev_args = list())
   args$nframes <- nframes %?% chunk_args$nframes %||% getOption('gganimate.nframes', 100)
   args$fps <- fps %?% chunk_args$fps %||% getOption('gganimate.fps', 10)
   duration <- duration %?% chunk_args$duration %||% getOption('gganimate.duration', NULL)
