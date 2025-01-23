310aa328307dbd0af37934f88d15fc3d457a7675
Teun van den Brand <tahvdbrand@gmail.com>
2025-01-23 11:08:03 +0100

    include forgotten else clause

--- R/plot-build.R
+++ R/plot-build.R
@@ -115,6 +115,8 @@ ggplot_build.gganim <- function(plot) {
     } else if (new_guides) {
       lapply(data, npscales$train_df)
       plot$guides <- plot$guides$build(npscales, plot$layers, plot$labels, data)
+    } else {
+      lapply(data, npscales$train_df)
     }
     data <- lapply(data, npscales$map_df)
   }
